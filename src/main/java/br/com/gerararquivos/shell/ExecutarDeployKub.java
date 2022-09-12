f49adffedd7af22c443e75b7af21ac7bda83c0de
Maicon Gerardi <maicon.gerardi@gmail.com>
2022-09-12 15:25:20 -0300

    ajuste para controlar portas automaticamente

--- src/main/java/br/com/gerararquivos/shell/ExecutarDeployKub.java
+++ src/main/java/br/com/gerararquivos/shell/ExecutarDeployKub.java
@@ -6,7 +6,6 @@ import java.util.concurrent.Executors;
 public class ExecutarDeployKub {
     public static void executarDeployKub(String image, String workspace) throws IOException, InterruptedException {
         ProcessBuilder builder = new ProcessBuilder();
-
         builder.command("sudo", "sh", "/usr/local/bin/upkub/pull-minikube.sh", image, workspace);
         Process process = builder.start();
         StreamGobbler streamGobbler =
